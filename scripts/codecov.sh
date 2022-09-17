47f461419411b146305c16f9c3bf3f8bfd2a06ac
Amichay Oren <amichay@sgeltd.com>
2022-09-17 12:35:33 +0300

    code-coverage : run all tests

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -3,10 +3,7 @@
 export CODECOV_TOKEN=260f22ca-4d24-408f-8d18-b67c1f294c41
 
 echo "Generate Coverage Report"
-python3 -m pytest -s --collect-only --cov --cov-report xml
-
-echo "Convert report to XML"
-coverage xml
+python3 -m pytest -s --cov --cov-report xml
 
 echo "upload coverage report"
 codecov -vt $CODECOV_TOKEN
